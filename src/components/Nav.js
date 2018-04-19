b8edb6b95f22cdccddbf803eaab911ec5f114980
Angus <angusclark93@gmail.com>
2018-04-19 12:38:28 +0100

    import correct image path for webpack render in component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,28 +8,36 @@ import './Nav.css';
 export default class Nav extends React.Component {
 
 render() {
+
+  let cats = {
+    cat1: require('../images/cat.jpg'),
+    cat2: require('../images/cat2.jpg'),
+    cat3: require('../images/cat3.jpg'),
+    cat4: require('../images/cat4.jpg')
+  }
+
     return (
       <div className="nav">
       <Menu widths = '4'>
         <Menu.Item
           name='Cat 1'
           active= {this.props.activeItem === 'Cat 1'}
-          onClick={() => this.props.changeImage('/static/media/cat.9a205e6f.jpg', 'Cat 1')}
+          onClick={() => this.props.changeImage(cats.cat1, 'Cat 1')}
       />
         <Menu.Item
           name= 'Cat 2'
           active={this.props.activeItem === 'Cat 2'}
-          onClick={() => this.props.changeImage('/static/media/cat2.ff86d15c.jpg', 'Cat 2')}
+          onClick={() => this.props.changeImage(cats.cat2, 'Cat 2')}
         />
         <Menu.Item
           name= 'Cat 3'
           active={this.props.activeItem === 'Cat 3'}
-          onClick={() => this.props.changeImage('/static/media/cat3.027a8443.jpg', 'Cat 3')}
+          onClick={() => this.props.changeImage(cats.cat3, 'Cat 3')}
         />
         <Menu.Item
           name= 'Cat 4'
           active={this.props.activeItem === 'Cat 4'}
-          onClick={() => this.props.changeImage('/static/media/cat4.b233fa59.jpg', 'Cat 4')}
+          onClick={() => this.props.changeImage(cats.cat4, 'Cat 4')}
         />
       </Menu>
       </div>
